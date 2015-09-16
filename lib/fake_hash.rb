e8cad56c2e382eec437b22d96c77e604be7bc304
elainapolson <elainapolson@gmail.com>
2015-09-16 14:42:10 -0400

    finished

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -2,4 +2,31 @@ class FakeHashWrapper
 
   # write code here
 
+  attr_accessor :hash
+
+  def initialize
+    self.hash = {}
+  end
+
+  def []=(key, value)
+    self.hash[key.to_sym] = value
+  end
+
+  def [](key)
+    self.hash[key.to_sym]
+  end
+
+  def keys
+    self.hash.keys.sort.reverse
+  end
+
+  def first_letter(letter)
+    keys = self.hash.keys.select {|key| key.to_s.split('').first == "a"}
+    keys.collect {|key| self.hash[key]}
+  end
+
+  def each
+    self.hash.each {|key, value| puts value}
+  end
+
 end
